e887ff8a174c449d8c53c324a5c0e9d61909e758
Jesse Lyon <jesse.a.lyon+github@gmail.com>
2019-10-17 20:05:23 +1100

    Fixed a few format errors

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,11 @@ import React, { Component } from "react"
 import Layout from '../components/layout'
 
 export default class index extends Component {
- render() {
-   return (
-    <Layout pageTitle="Home">
-      <h1>Home</h1>
-    </Layout>
-   )
+  render() {
+    return (
+      <Layout pageTitle="Home">
+        <h1>Home</h1>
+      </Layout>
+    )
   }
 }
